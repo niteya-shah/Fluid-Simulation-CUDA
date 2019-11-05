fba3b8ee36d8c198f1114efb768534ea04667e6f
Niteya <niteya.56@gmail.com>
2019-11-05 11:31:06 +0530

    final work for model

--- trial.py
+++ trial.py
@@ -37,7 +37,7 @@ __device__ float magnitude(const float *r) {
 }
 
 __device__ float magnitude_withId(const float *r) {
-  const int i = threadIdx.x;
+  const int i = blockIdx.x;
   return pow((r[3 * i] * r[3 * i]) + (r[3 * i + 1] * r[3 * i + 1]) + (r[3 * i + 2] * r[3 * i + 2]), 0.5);
 }
 
@@ -102,14 +102,14 @@ __device__ float lap_Wviscosity(const float mag, const float *h,
 
 __device__ void pressure_force(float *press_force, const float *mass,const float mag,
                                const float *r, const float *h,
-                               const float *density_p, const float *density_n,
+                                const float *density_n,const int *neighbors,
                                const float *rest_density, const float *k,
                                const float *grad_Wspiky_const) {
-  const int i = threadIdx.x;
+  const int i = neighbors[blockIdx.x * blockDim.x + threadIdx.x];
   float grad[3];
   grad_WSpiky(grad, mag, r, h, grad_Wspiky_const);
   float const_val =
-      *mass * *k * (*density_p + density_n[i] - 2 * *rest_density) * 1/(2 * density_n[i]);
+      *mass * *k * (density_n[blockIdx.x] + density_n[i] - 2 * *rest_density) * 1/(2 * density_n[i]);
   press_force[0] = const_val * grad[0];
   press_force[1] = const_val * grad[1];
   press_force[2] = const_val * grad[2];
@@ -118,36 +118,37 @@ __device__ void pressure_force(float *press_force, const float *mass,const float
 __device__ void viscosity_force(float *visc_force, const float *eta,
                                 const float *mass, const float mag,
                                 const float *h, const float *density_n,
-                                const float *vel_n, const float *vel_p,
+                                const float *vel_n,const int *neighbors,
                                 const float *lap_Wviscosity_const) {
-  const int i = threadIdx.x;
+
+  const int i = neighbors[blockIdx.x * blockDim.x + threadIdx.x];
   float const_val = *eta * *mass * (1 / (density_n[i] +  0.0000001f)) *
                     lap_Wviscosity(mag, h, lap_Wviscosity_const);
-  visc_force[0] = const_val * (vel_n[3 * i] - vel_p[0]);
-  visc_force[1] = const_val * (vel_n[3 * i + 1] - vel_p[1]);
-  visc_force[2] = const_val * (vel_n[3 * i + 2] - vel_p[2]);
+  visc_force[0] = const_val * (vel_n[3 * i] - vel_n[3 * blockIdx.x]);
+  visc_force[1] = const_val * (vel_n[3 * i + 1] - vel_n[3 * blockIdx.x + 1]);
+  visc_force[2] = const_val * (vel_n[3 * i + 2] - vel_n[3 * blockIdx.x + 2]);
 }
 
 __device__ void color_field_grad(float *color_field_grad_val, const float *mass,
-                                 const float *density_n, float *r,
+                                 const float *density_n, float *r_dash,const int *neighbors,
                                  const float *h, const float mag,
                                  const float *grad_WPoly6_const) {
-  const int i = threadIdx.x;
+  const int i = neighbors[blockIdx.x * blockDim.x + threadIdx.x];
   float inner_val = *mass * 1 / (density_n[i] +  0.0000001f);
   float grad_Wpoly[3];
-  grad_WPoly6(grad_Wpoly, r, mag, h, grad_WPoly6_const);
-  color_field_grad_val[3 * i] = grad_Wpoly[0] * inner_val;
-  color_field_grad_val[3 * i + 1] = grad_Wpoly[1] * inner_val;
-  color_field_grad_val[3 * i + 2] = grad_Wpoly[2] * inner_val;
+  grad_WPoly6(grad_Wpoly, r_dash, mag, h, grad_WPoly6_const);
+  color_field_grad_val[blockIdx.x * blockDim.x * 3 + 3 * threadIdx.x] = grad_Wpoly[0] * inner_val;
+  color_field_grad_val[blockIdx.x * blockDim.x * 3 + 3 * threadIdx.x + 1] = grad_Wpoly[1] * inner_val;
+  color_field_grad_val[blockIdx.x * blockDim.x * 3 + 3 * threadIdx.x + 2] = grad_Wpoly[2] * inner_val;
 }
 
 __device__ void color_field_lap(float *color_field_lap_val, const float *mass,
-                                const float *density_n, const float *r,
+                                const float *density_n, const float *r,const int *neighbors,
                                 const float *h, const float mag,
                                 const float *lap_WPoly6_const) {
-  const int i = threadIdx.x;
+  const int i = blockIdx.x * blockDim.x + threadIdx.x;
   color_field_lap_val[i] =
-      *mass * 1 / (density_n[i] +  0.0000001f) * lap_WPoly6(h, mag, lap_WPoly6_const);
+      *mass * 1 / (density_n[neighbors[i]] +  0.0000001f) * lap_WPoly6(h, mag, lap_WPoly6_const);
 }
 
 __global__ void calc_density(float *density,const float *r1,const int *neighbors,const float *mass,const float *h,const float *WPoly6_const)
@@ -156,49 +157,53 @@ __global__ void calc_density(float *density,const float *r1,const int *neighbors
       const int i = threadIdx.x;
       const int j = blockIdx.x;
       const int n = j * blockDim.x + i;
-      r_dash[0] = r1[j] - r1[3 * neighbors[n]];
-      r_dash[1] = r1[j + 1] - r1[3 * neighbors[n] + 1];
-      r_dash[2] = r1[j + 2] - r1[3 * neighbors[n] + 2];
+      r_dash[0] = r1[3 * j] - r1[3 * neighbors[n]];
+      r_dash[1] = r1[3 * j + 1] - r1[3 * neighbors[n] + 1];
+      r_dash[2] = r1[3 * j + 2] - r1[3 * neighbors[n] + 2];
       const float mag = magnitude(r_dash);
       density[n] = WPoly6(mag, h, WPoly6_const) * *mass;
 }
 
 __global__ void calc_forces(
     float *force, float *color_field_lap_val, float *color_field_grad_val,
-    const float *r, const float *r1, const float *h, const float *eta,
-    const float *mass, const float *density_p, const float *density_n,
-    const float *rest_density, const float *k, float *vel_n, float *vel_p,
+    const float *r1,const float *density_n,float *vel_n, int *neighbors,
+    const float *h, const float *eta,const float *mass,
+    const float *rest_density, const float *k,
     const float *grad_WPoly6_const, const float *lap_WPoly6_const,
     const float *Wspiky_const, const float *grad_Wspiky_const,
     const float *Wviscosity_const, const float *lap_Wviscosity_const) {
+
   const int i = threadIdx.x;
+  const int j = blockIdx.x;
+  const int n = j * blockDim.x + i;
+
   float r_dash[3], press_force[3], visc_force[3];
-  r_dash[0] = r[0] - r1[3 * i];
-  r_dash[1] = r[1] - r1[3 * i + 1];
-  r_dash[2] = r[2] - r1[3 * i + 2];
+  r_dash[0] = r1[3 * j] - r1[3 * neighbors[n]];
+  r_dash[1] = r1[3 * j + 1] - r1[3 * neighbors[n] + 1];
+  r_dash[2] = r1[3 * j + 1] - r1[3 * neighbors[n] + 2];
   float mag = magnitude(r_dash);
   if(mag > 0.000001f && mag < *h){
-  pressure_force(press_force, mass, mag, r_dash, h, density_p, density_n,
+  pressure_force(press_force, mass, mag, r_dash, h, density_n, neighbors,
                  rest_density, k, grad_Wspiky_const);
-  viscosity_force(visc_force, eta, mass, mag, h, density_n, vel_n, vel_p,
+  viscosity_force(visc_force, eta, mass, mag, h, density_n, vel_n,neighbors,
                   lap_Wviscosity_const);
-  color_field_grad(color_field_grad_val, mass, density_n, r_dash, h, mag,
+  color_field_grad(color_field_grad_val, mass, density_n, r_dash,neighbors ,h, mag,
                    grad_WPoly6_const);
-  color_field_lap(color_field_lap_val, mass, density_n, r_dash, h, mag,
+  color_field_lap(color_field_lap_val, mass, density_n, r_dash,neighbors ,h, mag,
                   lap_WPoly6_const);
-  force[3 * i] = clamp(press_force[0] + visc_force[0], 200, -200);
-  force[3 * i + 1] = clamp(press_force[1] + visc_force[1], 200, -200);
-  force[3 * i + 2] = clamp(press_force[2] + visc_force[2], 200, -200);
+  force[blockIdx.x * blockDim.x * 3 + 3 * i] = clamp(press_force[0] + visc_force[0], 200, -200);
+  force[blockIdx.x * blockDim.x * 3 + 3 * i + 1] = clamp(press_force[1] + visc_force[1], 200, -200);
+  force[blockIdx.x * blockDim.x * 3 + 3 * i + 2] = clamp(press_force[2] + visc_force[2], 200, -200);
   }
   else
   {
-    force[3 * i] = 0;
-    force[3 * i + 1] = 0;
-    force[3 * i + 2] = 0;
-    color_field_grad_val[3 * i] = 0;
-    color_field_grad_val[3 * i + 1] = 0;
-    color_field_grad_val[3 * i + 2] = 0;
-    color_field_lap_val[i] = 0;
+    force[blockIdx.x * blockDim.x * 3 + 3 * i] = 0;
+    force[blockIdx.x * blockDim.x * 3 + 3 * i + 1] = 0;
+    force[blockIdx.x * blockDim.x * 3 + 3 * i + 2] = 0;
+    color_field_grad_val[blockIdx.x * blockDim.x * 3 + 3 * i] = 0;
+    color_field_grad_val[blockIdx.x * blockDim.x * 3 + 3 * i + 1] = 0;
+    color_field_grad_val[blockIdx.x * blockDim.x * 3 + 3 * i + 2] = 0;
+    color_field_lap_val[blockIdx.x * blockDim.x + threadIdx.x] = 0;
   }
 }
 
@@ -207,7 +212,7 @@ __global__ void update_pos(float *r, float *vel_p, float *force,
                            const float *time, const float *sigma,const float *Width, const float *damping,const float *eps,
                            const float *color_field_lap_val,
                            const float *color_field_grad_val) {
-  const int i = threadIdx.x;
+  const int i = blockIdx.x;
   float gradient_length = magnitude_withId(color_field_grad_val);
   float force_surface[3] = {0,0,0};
 
@@ -272,7 +277,7 @@ v = np.zeros([const.num_particles,3]).astype(np.float32)
 plt.ion()
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-for iteration in range(40):
+for iteration in range(100):
     plt.xlim(-10,10)
     plt.ylim(-10, 10)
     ax.set_zlim(-10,10)
@@ -287,31 +292,23 @@ for iteration in range(40):
     density = np.zeros([const.num_particles,const.num_neighbhours]).flatten().astype(np.float32)
     calc_density(drv.Out(density), drv.In(r_dash),drv.In(neighbors),drv.In(const.mass) ,drv.In(const.h), drv.In(const.WPoly6_const),block=(const.num_neighbhours,1,1), grid=(const.num_particles,1))
     Density = density.reshape([const.num_particles, const.num_neighbhours]).sum(axis = 1)
-    print("dens")
-    force = np.zeros([const.num_particles,3]).astype(np.float32)
-    color_field_lap_val = np.zeros([const.num_particles]).astype(np.float32)
-    color_field_grad_val = np.zeros([const.num_particles,3]).astype(np.float32)
-    print("bfor force")
-    for i in range(const.num_particles):
-        force_temp = np.zeros([const.num_neighbhours,3]).flatten().astype(np.float32)
-        color_field_lap_val_temp = np.zeros([const.num_neighbhours]).astype(np.float32)
-        color_field_grad_val_temp = np.zeros([const.num_neighbhours,3]).flatten().astype(np.float32)
-        r = r1[i].astype(np.float32)
-        r_dash = r1[neighbors[i]].flatten().astype(np.float32)
-        density_p = np.float32(Density[i])
-        v_n = v[neighbors[i]].flatten().astype(np.float32)
-        v_p = v[i].astype(np.float32)
-        calc_forces(drv.Out(force_temp), drv.Out(color_field_lap_val_temp), drv.Out(color_field_grad_val_temp), drv.In(r), drv.In(r1), drv.In(const.h), drv.In(const.eta), drv.In(const.mass), drv.In(density_p), drv.In(Density), drv.In(const.rest_density), drv.In(const.k), drv.In(v_n), drv.In(v_p), drv.In(const.grad_WPoly6_const),drv.In(const.lap_WPoly6_const),drv.In(const.Wspiky_const),drv.In(const.grad_Wspiky_const),drv.In(const.Wviscosity_const),drv.In(const.lap_Wviscosity_const), block=(const.num_neighbhours,1,1), grid=(1,1))
-        force[i] = force_temp.reshape([const.num_neighbhours,3]).sum(axis = 0)
-        color_field_lap_val[i] = color_field_lap_val_temp.sum()
-        color_field_grad_val[i] = color_field_grad_val_temp.reshape([const.num_neighbhours,3]).sum(axis = 0)
-    print("after force")
+
+    force = np.zeros([const.num_particles,const.num_neighbhours,3]).astype(np.float32)
+    color_field_lap_val = np.zeros([const.num_particles, const.num_neighbhours]).astype(np.float32)
+    color_field_grad_val = np.zeros([const.num_particles, const.num_neighbhours, 3]).astype(np.float32)
+
+    r_dash = r1.flatten().astype(np.float32)
+    v_n = v.flatten().astype(np.float32)
+    calc_forces(drv.Out(force), drv.Out(color_field_lap_val), drv.Out(color_field_grad_val), drv.In(r1),  drv.In(Density), drv.In(v_n), drv.In(neighbors_dash) ,drv.In(const.h), drv.In(const.eta), drv.In(const.mass), drv.In(const.rest_density), drv.In(const.k),  drv.In(const.grad_WPoly6_const),drv.In(const.lap_WPoly6_const),drv.In(const.Wspiky_const),drv.In(const.grad_Wspiky_const),drv.In(const.Wviscosity_const),drv.In(const.lap_Wviscosity_const), block=(const.num_neighbhours,1,1), grid=(const.num_particles,1))
+    force = force.reshape([const.num_particles, const.num_neighbhours, 3]).sum(axis = 1)
+    color_field_grad_val = color_field_grad_val.reshape([const.num_particles, const.num_neighbhours, 3]).sum(axis = 1)
+    color_field_lap_val = color_field_lap_val.reshape([const.num_particles, const.num_neighbhours]).sum(axis = 1)
+
     r_dash = r1.flatten().astype(np.float32)
     v_n = v.flatten().astype(np.float32)
     force = force.flatten().astype(np.float32)
     color_field_grad_val = color_field_grad_val.flatten().astype(np.float32)
-    print("update_pos")
-    update_pos(drv.InOut(r_dash), drv.InOut(v_n), drv.In(force), drv.In(const.threshold),drv.In(const.mass),drv.In(const.time) ,drv.In(const.sigma),drv.In(const.Width),drv.In(const.damping),drv.In(const.eps) ,drv.In(color_field_lap_val), drv.In(color_field_grad_val), block=(const.num_particles,1,1), grid=(1,1))
-    print("final")
+
+    update_pos(drv.InOut(r_dash), drv.InOut(v_n), drv.In(force), drv.In(const.threshold),drv.In(const.mass),drv.In(const.time) ,drv.In(const.sigma),drv.In(const.Width),drv.In(const.damping),drv.In(const.eps) ,drv.In(color_field_lap_val), drv.In(color_field_grad_val), block=(1,1,1), grid=(const.num_particles,1))
     r1 = r_dash.reshape(const.num_particles,3)
     v = v_n.reshape(const.num_particles,3)
